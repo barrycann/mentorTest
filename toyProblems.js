bff68fbffe7d1ac2c308c92938821ce12f651a7e
Barry Cann <barrycann@gmail.com>
2017-03-29 13:21:10 -0600

    Completed the test

--- toyProblems.js
+++ toyProblems.js
@@ -1,46 +1,60 @@
 //JS Basics
 //Run npm test in the command line to test your solutions
 module.exports = {
-  // reverseIt:
-
-  //uncomment and finish the reverseIt function. It will take in one parameter which is a String and
-  //reverse it
-
-  // removeDups:
-
-  //uncomment and finish the removeDups function. It will take in one parameter which is an Array
-  //remove all duplicates
-
-  // titleIt:
-
-  //uncomment and finish the titleIt function. It will take in one parameter which is a String and
-  //capitalize the first letter of each word
-
-  // vowelCounter:
-
-  //uncomment and finish the vowelCounter function. It will take in one parameter which is a String and
-  //return the number of vowels in the string
-
-  // isPrime:
-
-  //uncomment and finish the isPrime function. It will take in one parameter which is a Number and
-  //return true if it is prime and false if it is not
-
-  //what is the value of foo?
-  //var foo = 10 + '20';
-  //uncomment the foo property and give your answer as its value
-
-  //foo:
-
-  //what is the outcome of the two console.logs below?
-  //   var foo = "Hello";
-  // (function() {
-  //   var bar = " World";
-  //   console.log(foo + bar);
-  // })();
-  // console.log(foo + bar);
-  // uncomment the log1 and log2 properties and give your answers as their values
-
-  // log1:
-  // log2:
+  reverseIt: function(str){
+    let strArr = str.split("");
+    let newArr = [];
+    for(var i=strArr.length-1; i>=0; i--){
+      newArr.push(strArr[i]);
+    }
+    finalArr = newArr.join("");
+    return finalArr;
+},
+  removeDups: function(arr){
+    for(var i=0; i<arr.length;i++){
+      for(var j=i+1; j<arr.length-1;j++){
+        if(arr[j] === arr[i]){
+          arr.splice(j, 1);
+          j--;
+        }
+      }
+    }
+    return arr;
+  },
+  titleIt: function(str){
+    var wordArr = str.split(" ");
+    var newSentence = [];
+    for(var i of wordArr){
+      let letters = i.split("");
+      letters[0] = letters[0].toUpperCase();
+      let newWord = letters.join("");
+      newSentence.push(newWord);
+    }
+    let finalSentence = newSentence.join(" ");
+    return finalSentence;
+  },
+  vowelCounter: function(str){
+    var strArr = str.split("");
+    var vowels = 0;
+    for(var i of strArr){
+      if(i == 'a' || i == 'e' || i == 'i' || i == 'o' || i == 'u'){
+        vowels ++;
+      }
+    }
+    return vowels;
+  },
+  isPrime: function(num){
+    if(num >= 1){
+      for(var i=num-1; i>1; i--){
+        if(num % i == 0){
+          return false;
+        }
+      }
+      return true;
+    }
+    return false;
+  },
+  foo: '1020',
+  log1: 'Hello World',
+  log2: undefined
 }
